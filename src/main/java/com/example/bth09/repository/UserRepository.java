59704d8c7d215bee190469fed22236b2540365f0
buihaiduongdev <haiduongtinhoc@gmail.com>
2025-09-27 18:54:15 +0700

    c

--- src/main/java/com/example/bth09/repository/UserRepository.java
+++ src/main/java/com/example/bth09/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.bth09.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.bth09.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-	User findByUsername(String username);
+	Optional<User> findByEmail(String email);
 }
